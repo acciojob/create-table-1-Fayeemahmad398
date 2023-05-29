2048c410e38db42e05695b181c4fad98789a7235
Fayeemahmad398 <117560978+Fayeemahmad398@users.noreply.github.com>
2023-05-29 12:22:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
+	let table=document.getElementById("sampleTable");
+	let row=table.insertRow(0);
+	let td1=row.insertCell(0);
+	let td2=row.insertCell(1);
+	td1.innerHTML="New Cell1";
+	td2.innerHTML="New Cell2";
+	
   
   
 }
